72b59980fcde431db6080576dad929231e64b1b7
kptankhoa <kptankhoa@gmail.com>
2021-06-03 17:41:47 +0700

    fix search controller

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,10 +1,21 @@
 "use strict";
 const searchService = require("../services/search.service");
 module.exports = {
-  getCourses: async (req, res, next) => {
+  getCoursesByName: async (req, res, next) => {
     const filter = {};
     filter.keyword = req.query.keyword;
+    filter.page = +req.query.page || 1;
+    filter.sortBy = req.query.sortBy;
     const courses = await searchService.getCoursesByFilter(filter);
+    res.json(courses);
+  },
+
+  getCoursesByCategory: async (req, res, next) => {
+    const filter = {};
+    filter.keyword = req.query.keyword;
+    filter.page = +req.query.page || 1;
+    filter.sortBy = req.query.sortBy;
+    const courses = await searchService.getCoursesByCate(filter);
     res.json(courses);
   }
 };
