a9bcc9076c57a47cacdf3e41b14ffc7a3dd1a9f6
Gia-Kiet-99 <kietdg99@gmail.com>
2021-06-03 22:02:18 +0700

    Implement Category API: Get featured category of the week

--- services/category.service.js
+++ services/category.service.js
@@ -1,5 +1,6 @@
 "use strict";
 const categoryModel = require("../models/category.model");
+const enrollmentModel = require("../models/enrollment.model");
 
 module.exports = {
   /**
@@ -12,5 +13,38 @@ module.exports = {
 
   getCategoryByCategoryId: async (categoryId) => {
     return await categoryModel.findById(categoryId);
+  },
+
+  getFeatured: async () => {
+    try {
+      const aWeek = 7 * 24 * 60 * 60 * 1000;
+      const startDate = new Date(Date.now() - aWeek);
+
+      const enrollments = await enrollmentModel.find({
+        registeredTime: {
+          $gte: startDate
+        }
+      }).populate({
+        path: "courseId",
+        select: "category"
+      });
+      // console.log(enrollment);
+
+      const categoryIds = enrollments.map(e => e.courseId.category)
+        // remove duplicate
+        .filter((id, index, arr) => arr.indexOf(id) === index);
+      // console.log(categoryIds);
+
+      const categories = await categoryModel.find({
+        _id: {
+          $in: categoryIds
+        },
+        isDeleted: false
+      }).select("categoryName");
+      // console.log(categories);
+      return categories;
+    } catch (error) {
+      throw Error(error);
+    }
   }
 };
