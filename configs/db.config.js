8e4f2356b976f9c31a6e88b3b43ae8ce77be591c
GitHubVanlinh <vovanlinhleo1999@gmail.com>
2021-06-04 18:25:55 +0700

    format test

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,18 +1,22 @@
 "use strict";
 const mongoose = require("mongoose");
 
-const url = (process.env.NODE_ENV != "test")?process.env.MONGODB_URL:process.env.TEST_MONGODB_URL
+const url =
+  process.env.NODE_ENV != "test"
+    ? process.env.MONGODB_URL
+    : process.env.TEST_MONGODB_URL;
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+module.exports = new Promise((resolve, reject) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", () => {
+    // we're connected!
+    console.log("Connect database successfully!");
+    resolve();
+  });
 });
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  // we're connected!
-  console.log("Connect database successfully!");
-});
-
-module.exports = db;
--- tests/user.test.js
+++ tests/user.test.js
@@ -9,63 +9,69 @@ const UserModel = require("../models/user.model");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../app");
+const { run } = require("mocha");
 const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("Users", () => {
-  beforeEach(async () => {
-    await UserModel.deleteMany({}, {});
-  });
-  // beforeEach((done) => {
-  //   UserModel.deleteMany({}, {}, (err) => {
-  //     done();
-  //   });
-  // });
-
-  describe("/POST ", () => {
-    it("it should create a student", async () => {
-      const user = {
-        username: "abc",
-        password: "abc",
-        fullName: "vanlinh",
-        email: "vovanlinhleo1999@gmail.com",
-        avatar: "http://abc.ch"
-      };
-      const res = await chai
-        .request(app)
-        .post("/users")
-        .send(user);
-      should.exist(res);
-      should.exist(res.body);
-
-      res.should.have.status(201);
-      res.body.should.be.a("object");
-      res.body.should.have.property("status").eql("PENDING");
+setTimeout(() => {
+  describe("Users",async () => {
+    before(async () => {
+      await require("../configs/db.config");
     });
+    
+    beforeEach(async () => {
+      await UserModel.deleteMany({}, {});
+    });
+    // beforeEach((done) => {
+    //   UserModel.deleteMany({}, {}, (err) => {
+    //     done();
+    //   });
+    // });
+
+    describe("/POST ", () => {
+      it("it should create a student", async () => {
+        const user = {
+          username: "abc",
+          password: "abc",
+          fullName: "vanlinh",
+          email: "vovanlinhleo1999@gmail.com",
+          avatar: "http://abc.ch"
+        };
+        const res = await chai.request(app).post("/users").send(user);
+        should.exist(res);
+        should.exist(res.body);
+
+        res.should.have.status(201);
+        res.body.should.be.a("object");
+        res.body.should.have.property("status").eql("PENDING");
+      });
+    });
+    /*
+     * Test the /POST route
+     */
+    // describe("/POST book", () => {
+    //   it("it should not POST a book without pages field", (done) => {
+    //     const book = {
+    //       title: "The Lord of the Rings",
+    //       author: "J.R.R. Tolkien",
+    //       year: 1954
+    //     };
+    //     chai
+    //       .request(app)
+    //       .post("/book")
+    //       .send(book)
+    //       .end((err, res) => {
+    //         res.should.have.status(200);
+    //         res.body.should.be.a("object");
+    //         res.body.should.have.property("errors");
+    //         res.body.errors.should.have.property("pages");
+    //         res.body.errors.pages.should.have.property("kind").eql("required");
+    //         done();
+    //       });
+    //   });
+    // });
   });
-  /*
-   * Test the /POST route
-   */
-  // describe("/POST book", () => {
-  //   it("it should not POST a book without pages field", (done) => {
-  //     const book = {
-  //       title: "The Lord of the Rings",
-  //       author: "J.R.R. Tolkien",
-  //       year: 1954
-  //     };
-  //     chai
-  //       .request(app)
-  //       .post("/book")
-  //       .send(book)
-  //       .end((err, res) => {
-  //         res.should.have.status(200);
-  //         res.body.should.be.a("object");
-  //         res.body.should.have.property("errors");
-  //         res.body.errors.should.have.property("pages");
-  //         res.body.errors.pages.should.have.property("kind").eql("required");
-  //         done();
-  //       });
-  //   });
-  // });
-}).timeout(10000);
+
+  run();
+}, 10000);
