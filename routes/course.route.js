7ddc14a6149643427cade697c7ca6e35cb5bd421
Gia-Kiet-99 <kietdg99@gmail.com>
2021-06-07 11:37:55 +0700

    Implement course API: get lesson info

--- routes/course.route.js
+++ routes/course.route.js
@@ -2,12 +2,15 @@
 const express = require("express");
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const Validator = require("../middlewares/validator.mdw");
 
+const Validator = require("../middlewares/validator.mdw");
 const paramsValidate = require("../middlewares/paramscheck.mdw");
 const queryValidate = require("../middlewares/querycheck.mdw");
 
 const CourseController = require("../controllers/course.controller");
+const LessonController = require("../controllers/lesson.controller");
+
+const auth = require("../middlewares/auth.mdw");
 
 router.get("/", queryValidate.checkCategoryId, CourseController.getCourses);
 
@@ -17,5 +20,8 @@ router.get("/:courseId/lecturers", CourseController.getLecturersOfCourse);
 router.get("/:courseId/feedbacks", CourseController.getFeedbacksOfCourse);
 // send feedback
 router.post("/:courseId/feedback", Validator.validateRequestBody("send_feedback"), CourseController.addFeedback);
+
+// get lesson info
+router.get("/:courseId/lessons/:lessonId", auth, LessonController.getLessonById);
 
 module.exports = router;
