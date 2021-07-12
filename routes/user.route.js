71ad343d5fafb8e98de7711ce72194fc48b48967
GitHubVanlinh <vovanlinhleo1999@gmail.com>
2021-07-12 13:52:19 +0700

    add get user

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,29 +6,38 @@ const upload = require("../configs/multer.config");
 const UserController = require("../controllers/user.controller");
 const Validator = require("../middlewares/validator.mdw");
 
+router.post(
+  "/",
+  Validator.validateRequestBody("register_student"),
+  UserController.createNewStudent
+);
 
-router.post("/", Validator.validateRequestBody("register_student"), UserController.createNewStudent);
+router.get("/:userId", UserController.getUser);
 
 // update user info (fullName, phone, address)
-router.patch("/:userId",
+router.patch(
+  "/:userId",
   Validator.validateRequestBody("update_user_info"),
   UserController.updateUserInfo
 );
 
 // change user password
-router.patch("/:userId/password",
+router.patch(
+  "/:userId/password",
   Validator.validateRequestBody("update_user_password"),
   UserController.updateUserPassword
 );
 
 // change user email
-router.post("/:userId/email",
+router.post(
+  "/:userId/email",
   Validator.validateRequestBody("update_user_email"),
   UserController.makeEmailVerification
 );
 
 // verify change email
-router.post("/:userId/verify",
+router.post(
+  "/:userId/verify",
   Validator.validateRequestBody("validate_student"),
   UserController.verifyAndUpdateEmail
 );
@@ -46,7 +55,10 @@ router.patch("/:userId/wishList", UserController.deleteUserCoursesFromWishList);
 router.get("/:userId/registeredList", UserController.getUserRegisteredList);
 
 // user change avatar
-router.post("/:userId/avatar", upload.single("avaImage"), UserController.changeAvatar);
-
+router.post(
+  "/:userId/avatar",
+  upload.single("avaImage"),
+  UserController.changeAvatar
+);
 
 module.exports = router;
